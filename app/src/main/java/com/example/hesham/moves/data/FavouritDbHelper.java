4c45df30e51d6bdb619bc0deb7a17d87be01f48d
hmostafa3bde <hmostafa3bde@outlook.com>
2017-10-17 20:21:43 +0200

    stage2 content provieder

--- app/src/main/java/com/example/hesham/moves/data/FavouritDbHelper.java
+++ app/src/main/java/com/example/hesham/moves/data/FavouritDbHelper.java
@@ -3,6 +3,7 @@ package com.example.hesham.moves.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by Hesham on 10/17/2017.
@@ -10,13 +11,38 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class FavouritDbHelper extends SQLiteOpenHelper {
 
+    private static final String DATABASE_NAME= "favorite.db";
+    private static final int DATABASE_VERSION=1;
 
-    public FavouritDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    SQLiteOpenHelper dbhandler;
+    SQLiteDatabase db;
+
+    public FavouritDbHelper(Context context) {
+        super(context, DATABASE_NAME,null, DATABASE_VERSION);
     }
+
+    public void open() {
+        Log.d("Guinness","Databse open"+"--------->");
+        db= dbhandler.getWritableDatabase();
+    }
+    public void close() {
+        Log.d("Guinness","Databse close"+"--------->");
+         dbhandler.close();
+    }
+
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    final  String SQL_CREATE_FAVORITE_TABLE="CREATE TABLE "+FavouriteContract.FavoriteEntry.TABLE_NAME + " ("+
+            FavouriteContract.FavoriteEntry._ID+"INTEGER PRIMARY KEY AUTOINCREMENT, "+
+            FavouriteContract.FavoriteEntry.COLUMN_MOVIEID+"INTEGER, "+
+            FavouriteContract.FavoriteEntry.COLUMN_TITILE+"TEXT NOT NULL, "+
+            FavouriteContract.FavoriteEntry.COLUMN_USERRATING+"REAL NOT NULL, "+
+            FavouriteContract.FavoriteEntry.COLUMN_POSTPATH+"TEXT NOT NULL, "+
+            FavouriteContract.FavoriteEntry.COLUMN_PLOT_SYNOPSIS+"TEXT NOT NULL, "+"); ";
+
+
+        sqLiteDatabase.execSQL(SQL_CREATE_FAVORITE_TABLE);
 
     }
 
